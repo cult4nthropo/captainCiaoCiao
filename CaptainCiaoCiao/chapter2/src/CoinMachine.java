c252f74c7b8e7014d858e360571a3903cef02b9d
Kathleen <88250122+cult4nthropo@users.noreply.github.com>
2022-01-22 18:27:05 +0100

    next exercises

---
+++ CaptainCiaoCiao/chapter2/src/CoinMachine.java
@@ -0,0 +1,21 @@
+import java.awt.*;
+import java.util.Locale;
+
+public class CoinMachine {
+    public static void main(String[] args) {
+
+
+        System.out.println("How much money do we have Captain?");
+        double [] coinValue = {2.0, 1.0, 0.5, 0.2, 0.1, 0.05, 0.02, 0.01};
+        double moneyAmount = new java.util.Scanner(System.in).nextDouble();
+        String EURO = "\u20ac";
+
+        for (int i = 0; i < coinValue.length; i++) {
+            int coinCount = (int) (moneyAmount / coinValue[i]);
+            System.out.printf(Locale.GERMAN,"%d x %s %s %n", coinCount, coinValue[i], EURO);
+            moneyAmount -= coinCount * coinValue[i];
+        }
+
+
+    }
+}
---
+++ CaptainCiaoCiao/chapter2/src/SingularOrPlural.java
@@ -0,0 +1,8 @@
+public class SingularOrPlural {
+    public static void main(String[]args) {
+        System.out.println("How many bottles of rum do we have Captain?");
+        int noOfBottles = new java.util.Scanner(System.in).nextInt();
+        boolean plural = (noOfBottles != 1);
+        System.out.printf("%d bottle%s of rum", noOfBottles, plural ? "s" : "");
+    }
+}
