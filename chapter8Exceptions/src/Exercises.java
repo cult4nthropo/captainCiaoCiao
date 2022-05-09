64d06ba011d2c6300bf74983561723db32fa5dc1
Kathleen Prasatko <88250122+cult4nthropo@users.noreply.github.com>
2022-05-09 21:47:30 +0200

    try with ressources (PrintWriter)

--- chapter8Exceptions/src/Exercises.java
+++ chapter8Exceptions/src/Exercises.java
@@ -1,5 +1,9 @@
 package src;
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
+
 public class Exercises {
 
 	public static void main(String[] args) {
@@ -7,6 +11,15 @@ public class Exercises {
 		for (int number : numbers) {
 			System.out.println(number);
 		}
+
+		String fileName = "chapter8Exceptions\\dateFile.txt";
+
+		try (PrintWriter printWriter = new PrintWriter(fileName)) {
+			printWriter.write(LocalDateTime.now().toString());
+		} catch (FileNotFoundException e) {
+			System.err.printf("%s could not be created.", fileName);
+		}
+
 	}
 
 	static int[] parseInts(String... numbers) {
@@ -16,9 +29,9 @@ public class Exercises {
 				intArray[i] = Integer.parseInt(numbers[i]);
 			} catch (NumberFormatException e) {
 				/*
-				System.out.println("This cannot be converted to an integer");
-				e.printStackTrace();
-				*/
+				 * System.out.println("This cannot be converted to an integer");
+				 * e.printStackTrace();
+				 */
 				intArray[i] = 0;
 			}
 		}
