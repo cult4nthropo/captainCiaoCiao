cf70642cfdc80ef756787434784153732192dcfc
Kathleen Prasatko <88250122+cult4nthropo@users.noreply.github.com>
2022-05-11 21:13:58 +0200

    ReaderWriter clean up

--- chapter8Exceptions/src/ReaderWriter.java
+++ chapter8Exceptions/src/ReaderWriter.java
@@ -1,23 +1,23 @@
 package src;
 
-
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.file.Paths;
 import java.util.Scanner;
 
-public class NoteReader {
+public class ReaderWriter {
 
 	public static void main(String[] args) {
-		readFile();
-		writeFile();
+		readAndWriteFile();
 
 	}
 
-	public static String readFile() {
+	public static String readAndWriteFile() {
+		String source = "chapter8Exceptions\\file.txt";
+		String target = "chapter8Exceptions\\noteFile.txt";
 		StringBuilder redFile = new StringBuilder();
-		try (Scanner scanner = new Scanner(Paths.get("chapter8Exceptions\\file.txt"))) {
+		try (Scanner scanner = new Scanner(Paths.get(source)); PrintWriter printWriter = new PrintWriter(target)) {
 			while (scanner.hasNextLine()) {
 				String nextLine = scanner.nextLine();
 				String validNotes = "A, B, C, D, E, F, G, A B C D E F G a b c d e f g a' b' c' d' e' f' g'";
@@ -25,21 +25,13 @@ public class NoteReader {
 					redFile.append(nextLine + " ");
 				}
 			}
+			printWriter.write(redFile.toString());
+		} catch (FileNotFoundException e) {
+			System.err.printf("%s could not be created.", target);
 		} catch (IOException e) {
-			System.out.println("This file does not exist.");
+			System.err.println("This file does not exist.");
 			e.printStackTrace();
 		}
 		return redFile.toString();
 	}
-	
-	public static void writeFile() {
-		String fileName = "chapter8Exceptions\\noteFile.txt";
-
-		try (PrintWriter printWriter = new PrintWriter(fileName)) {
-			printWriter.write(readFile());;
-		} catch (FileNotFoundException e) {
-			System.err.printf("%s could not be created.", fileName);
-		}
-	}
-
 }
